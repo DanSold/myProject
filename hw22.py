b58e511668cfa14451134a967b182be25a0402cd
“danKo” <dansoldat9@gmail.com>
2023-05-14 19:55:16 +0300

    hw22

---
+++ hw22.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+
+class Car:
+
+    def __init__(self, mark, brand, fuel_consumption, graduation_year=2020):
+        self.graduation_year = graduation_year
+        self.mark = mark
+        self.brand = brand
+        self.fuel_consumption = float(fuel_consumption)
+        self.mileage = 0
+
+    def __str__(self):
+        return f'<Car> graduation_year: {self.graduation_year},\n' \
+               f'mark: {self.mark}\n' \
+               f'brand: {self.brand}\n' \
+               f'fuel_consumption: {self.fuel_consumption}\n' \
+               f'car_age: {self.determination_of_age_in_years}\n' \
+               f'mileage: {self.mileage}'
+
+    @property
+    def determination_of_age_in_years(self):
+        car_age = datetime.today().year - self.graduation_year
+        return car_age
+
+    def add_mileage(self, distance=0):
+        self.mileage += distance
+        return self.mileage
+
+
+car_1 = Car(graduation_year=2013, mark='Audi RS7', brand='Audi', fuel_consumption=11.6)
+car_2 = Car(mark='Porsche 911', brand='Porsche', fuel_consumption=9.4)
+car_3 = Car(graduation_year=2022, mark='Ferrari 296 GTB', brand='Ferrari', fuel_consumption=6.4)
+car_4 = Car(mark='Toyota Supra', brand='Toyota', fuel_consumption=9.4)
+car_5 = Car(graduation_year=2013, mark='Nissan GT-R', brand='Nissan', fuel_consumption=12)
