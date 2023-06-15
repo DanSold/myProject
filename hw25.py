6cbef4f6594be0e32bbe5b328d55f0bffb643ad9
“danKo” <dansoldat9@gmail.com>
2023-06-15 13:15:17 +0300

    1

--- hw25.py
+++ hw25.py
@@ -1,16 +1,16 @@
 import sqlite3
 
 products = [
-    ('Coca-Cola sugar', '250 ml, with sugar', 17.5),
-    ('Pepsi', '250 ml, no sugar', 20),
+    ('Coca-Cola sugar', '250 ml, with sugar', 9),
+    ('Pepsi', '250 ml, no sugar', 8),
     ('Fanta', '330 ml, no sugar, with juice', 22.2),
     ('Sprite sugar', '1 l, with sugar', 18.9),
     ('Waffles with sugar', '500 g', 33),
     ('Potato', '1 kg', 55),
     ('Tomato', '1 kg', 66),
-    ('Pineapples in sugar', '333 g', 100),
+    ('Pineapples in sugar', '333 g', 200),
     ('Cherry', '1 kg', 80),
-    ('Cherry in sugar', '333 kg', 90)
+    ('Cherry in sugar', '333 kg', 150)
 ]
 
 with sqlite3.connect('database.db') as connection:
@@ -32,3 +32,28 @@ with sqlite3.connect('database.db') as connection:
         """,
         products,
     )
+
+    data = cursor.execute("""
+        SELECT *
+        FROM product
+    """)
+
+    print(data.fetchmany(5))
+
+    second_data = cursor.execute("""
+        SELECT name, price
+        FROM product
+        WHERE
+            name LIKE '%sugar%' AND
+            price > 100
+    """)
+
+    print(second_data.fetchall())
+
+    cursor.execute("""
+        UPDATE product
+        SET
+            price = price + 12
+        WHERE
+            price < 10
+    """)
