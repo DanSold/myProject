f0d04c5dbe811a92adca26850a1d021c5abe31de
“danKo” <dansoldat9@gmail.com>
2023-07-19 13:56:28 +0300

    hw 29

--- hw27.py
+++ hw27.py
@@ -1,16 +1,17 @@
-import hashlib
-
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+import re
 
 
-def get_password_hash(password: str) -> str:
-    result = pwd_context.hash(password)
-    return result
-
-txt = "Company123"
-
-x = txt.isascii()
-
-print(x)
+def checking_validation_password(password: str) -> bool:
+    if len(password) < 8:
+        return False
+    if not any(char.isdigit() for char in password):
+        return False
+    if not any(char.islower() for char in password):
+        return False
+    if not any(char.isupper() for char in password):
+        return False
+    if not re.search(r'[+\-/*!"№;%:?()=]', password):
+        return False
+    if any(char.isspace() or not char.isascii() for char in password):
+        return False
+    return True
