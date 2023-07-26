b83e37990d998e87d1d632291de6c801b6bd14be
“danKo” <dansoldat9@gmail.com>
2023-07-26 20:07:04 +0300

    hw 32 final

--- hw32.py
+++ hw32.py
@@ -6,18 +6,32 @@ payload = {
     'name': 'Dan',
     'location': 'Ukraine',
     'password': 'dgkmsmkemmgddgdl;',
-    'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60),
+    'exp': datetime.datetime.utcnow() - datetime.timedelta(seconds=60),
     'iat': datetime.datetime.utcnow()
 }
 
-encode_jwt = jwt.encode(
-    payload=payload,
-    key=config.JWT_SECRET,
-    algorithm='HS256'
-)
 
-decoded = jwt.decode(
-    encode_jwt,
-    config.JWT_SECRET,
-    algorithms=['HS256'],
-)
+def encode_jwt(payload_dict: dict, algorithm: str) -> str:
+    result_encode_jwt = jwt.encode(
+        payload=payload_dict,
+        key=config.JWT_SECRET,
+        algorithm=algorithm
+    )
+    return result_encode_jwt
+
+
+def decoded_jwt(input_encode_jwt: str, algorithm: list) -> dict | None:
+    try:
+        decoded = jwt.decode(
+            input_encode_jwt,
+            config.JWT_SECRET,
+            algorithms=algorithm,
+        )
+        return decoded
+    except:
+        return None
+
+
+encode_jwt_item = encode_jwt(payload_dict=payload, algorithm='HS256')
+print(encode_jwt_item)
+print(decoded_jwt(input_encode_jwt=encode_jwt_item, algorithm=['HS256']))
