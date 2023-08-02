76a45d32891c293286e37c39336fa46d318e5374
“danKo” <dansoldat9@gmail.com>
2023-08-02 18:53:55 +0300

    hw 33

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ base_url = 'http://api.open-notify.org/iss-now.json'
 while True:
     response = requests.get(url=base_url)
     data = response.json()
-    result = f'{data["timestamp"]};{data["iss_position"]["latitude"]};{data["iss_position"]["longitude"]};'
+    result = f'{data["timestamp"]};{data["iss_position"]["latitude"]};{data["iss_position"]["longitude"]};\n'
     print(result)
     with open(f'result_data.csv', 'a') as file:
         file.write(result)
