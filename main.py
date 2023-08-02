2911743b1386fef55ceca728800c98c20fa2167e
“danKo” <dansoldat9@gmail.com>
2023-08-02 18:43:25 +0300

    hw 33

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+import time
+
+base_url = 'http://api.open-notify.org/iss-now.json'
+
+while True:
+    response = requests.get(url=base_url)
+    data = response.json()
+    result = f'{data["timestamp"]};{data["iss_position"]["latitude"]};{data["iss_position"]["longitude"]};'
+    print(result)
+    with open(f'result_data.csv', 'a') as file:
+        file.write(result)
+        time.sleep(5)
